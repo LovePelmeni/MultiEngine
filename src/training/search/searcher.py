92fcb237ccb2f62fb0fb0713304e1f30f1abfcd4
= <kirklimushin@gmail.com>
2024-03-26 11:49:19 +0300

    Added IVFPQ similarity search

--- src/training/search/searcher.py
+++ src/training/search/searcher.py
@@ -1,29 +1,197 @@
 import faiss
 import typing
+import abc
 import torch
+import numpy
+import logging
 
-class SimilarItemSearcher(object):
+logger = logging.getLogger(name='search_logger')
+file_handler = logging.FileHandler(filename="search_logs.log")
+logger.addHandler(file_handler)
+
+
+class BaseSimilaritySearcher(abc.ABC):
     """
-    Base module for searching
-    similar embeddings 
+    Base module for searching similar
+    embedding vectors, that are spatial
+    representations of products or items to recommend.
     """
+    def train(self, train_embeddings: numpy.ndarray):
+        """
+        In case Search Index is trainable,
+        we should provide method to train
+        it on a given set of 'embedding' vectors
+        """
+        if not self.index.is_trained:
+            if hasattr(self.index, 'add'):
+                self.index.add(train_embeddings)
+
+    @abc.abstractmethod
+    def search(self, embedding: torch.Tensor):
+        """
+        Performs index search to
+        get top K similar embeddings.
+        
+        Parameters:
+        ----------
+            embedding - target embedding
+            item_info - additional information about item.
+        """
+
+class FlatSearcher(BaseSimilaritySearcher):
+    """
+    Base module for searching similar 
+    embedding vectors using L2 euclidian distance norm
+    """
+    def __init__(self):
+        super(BaseSimilaritySearcher, self).__init__()
+
     @classmethod
     def from_config(cls, config: typing.Dict):
 
         number_of_suggestions = config.get("number_of_suggestions")
-        index_paths = config.get("index_paths")
+
+        if 'index_path' in config:
+            index_path = config.get("index_path")
+            cls.index  = faiss.read_index(index_path)
+        else:
+            dim = config.get("embedding_dim")
+            cls.index = faiss.IndexFlatL2(dim)
 
         cls.number_of_suggestions = number_of_suggestions
-        cls.indexes: typing.Dict[str, faiss.Index] = {
-            index_label: faiss.read_index(index_path)
-            for index_label, index_path in index_paths.items()
-        }
+
+        if not cls.index.is_trained:
+            if 'train_embeddings' in config:
+                train_embs = config.get('train_embeddings')
+                cls.index.add(train_embs)
         return cls()
         
-    def search(self, embedding: torch.Tensor, category: str) -> typing.List[int]:
-        _, candidate_indices = self.indexes[category].search(
-            x=embedding,
-            k=self.number_of_suggestions
+    def search(self, embedding: torch.Tensor) -> typing.List[int]:
+        _, candidate_indices = self.index.search(
+            embedding, 
+            self.number_of_suggestions
         )
         return candidate_indices
         
+class LSHSearcher(BaseSimilaritySearcher):
+    """
+    Module for finding similar embedding vectors
+    based on Local Similarity Hashing (LSH) algorithm.
+    """
+    def __init__(self):
+        super(BaseSimilaritySearcher, self).__init__()
+        
+    @classmethod
+    def from_config(cls, config: typing.Dict):
+
+        index_path = config.get("index_path")
+        num_of_suggestions = config.get("num_suggestions")
+
+        if 'index_path' in config:
+            cls.index = faiss.read_index(index_path)
+        else:
+            num_hash_tables = config.get("num_hash_tables")
+            embedding_dim = config.get("embedding_dim")
+            cls.index = faiss.IndexLSH(
+                embedding_dim, 
+                num_hash_tables
+            )
+
+        cls.number_of_suggestions = num_of_suggestions
+        
+        if not cls.index.is_trained and 'train_embeddings' in config:
+            train_embs = config.get("train_embeddings")
+            cls.index.add(train_embs)
+        return cls()
+
+    def search(self, embedding: torch.Tensor) -> typing.List:
+        _, candidate_indices = self.index.search(
+            embedding, 
+            self.number_of_suggestions
+        )
+        return candidate_indices
+
+class HNSWSearcher(BaseSimilaritySearcher):
+    """
+    Base module for finding similarity
+    between embedding vectors, based on (HNSW)
+    searching algorithm.
+    """
+    def __init__(self):
+        super(BaseSimilaritySearcher, self).__init__()
+
+    @classmethod
+    def from_config(cls, config: typing.Dict):
+
+        index_path = config.get("index_path")
+        num_of_suggestions = config.get("num_suggestions")
+
+        if 'index_path' in config:
+            cls.index = faiss.read_index(index_path)
+        else:
+            # Set HNSW index parameters
+            dim = config.get("embedding_dim")
+            M = config.get("vertex_connections") # number of connections each vertex will have
+            ef_search = config.get("ef_search") # depth of layers explored during search
+            ef_construction = config.get("ef_construction") # depth of layers explored during index construction
+
+            cls.index = faiss.IndexHNSW(dim, M)
+            cls.index.hnsw.efContruction = ef_construction
+            cls.index.hnsw.efSearch = ef_search
+
+        cls.number_of_suggestions = num_of_suggestions
+        
+        if not cls.index.is_trained:
+            if 'train_embeddings' in config:
+                train_embs = config.get("train_embeddings")
+                cls.index.add(train_embs)
+
+        return cls()
+
+    def search(self, embedding: torch.Tensor) -> typing.List:
+        _, candidate_indices = self.index.search(
+            embedding, 
+            self.number_of_suggestions
+        )
+        return candidate_indices
+
+class IVNFPQSimilaritySeacher(BaseSimilaritySearcher):
+    """
+    Base module for finding similar embedding vectors
+    using Inverted File Product Quantization Algorithm.
+    """
+    @classmethod
+    def from_config(cls, config: typing.Dict):
+        num_of_suggestions = config.get("num_suggestions")
+
+        if 'index_path' in config:
+            index_path = config.get("index_path")
+            cls.index = faiss.read_index(index_path)
+        else:
+            n_centroids = config.get("n_centroids")
+            code_size = config.get("code_size")
+            nbits = config.get("nbits")
+            embedding_dim = config.get('embedding_dim')
+
+            coarse_quantizer = faiss.IndexFlatL2(embedding_dim)
+            cls.index = faiss.IndexIVFPQ(
+                coarse_quantizer, 
+                embedding_dim, 
+                n_centroids, 
+                code_size, nbits
+            )
+        
+        if not cls.index.is_trained:
+            if 'train_embeddings' in config:
+                train_embs = config.get("train_embeddings")
+                cls.index.add(train_embs)
+
+        cls.number_of_suggestions = num_of_suggestions
+        return cls()
+
+    def search(self, embedding: torch.Tensor):
+        _, candidate_indices = self.index.search(
+            n=self.number_of_suggestions, 
+            x=embedding
+        )
+        return candidate_indices
