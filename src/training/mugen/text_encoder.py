161b2ea84ad916942879697537b512ef4b64c886
= <kirklimushin@gmail.com>
2024-04-07 13:38:34 +0300

    Renamed path

--- src/training/mugen/text_encoder.py
+++
@@ -1,9 +0,0 @@
-from torch import nn
-
-class TextEncoder(nn.Module):
-    """
-    DistilBERT-based Embedding Generation 
-    network for processing text sequences
-    """
-    def __init__(self):
-        super(TextEncoder, self).__init__()
