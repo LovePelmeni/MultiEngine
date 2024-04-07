8329cff4ef42780ac4bf7b00b9ed6a59e6c37b1e
= <kirklimushin@gmail.com>
2024-04-07 13:57:38 +0300

    tweaked samplers s

--- src/training/contrastive_learning/sampler.py
+++ src/training/contrastive_learning/sampler.py
@@ -16,22 +16,6 @@ class BaseSampler(ABC):
         textual_data - list textual sentence blocks
         audio_data - list of audio files
     """
-    def __init__(self, 
-        batch_size: int, 
-        video_data: typing.List[typing.Union[numpy.ndarray, torch.Tensor]], 
-        textual_data: typing.List[typing.Union[numpy.ndarray, torch.Tensor]], 
-        audio_data: typing.List[typing.Union[numpy.ndarray, torch.Tensor]],
-        labels: typing.List[int],
-        **kwargs
-    ):
-        super(BaseSampler, self).__init__(**kwargs)
-        self.batch_size = batch_size 
-        self.aligned_pairs, self.labels = self.align_modal_data(
-            video_data=video_data,
-            text_data=textual_data,
-            audio_data=audio_data,
-            labels=labels
-        )
 
     @abstractmethod
     def pair_similarity_metric(self, pair1, pair2, **kwargs):
@@ -85,15 +69,14 @@ class InstanceDiscriminationContrastSampler(BaseSampler):
 
             pos_sample = (
                 self.image_transformations(sample[0]) if self.video_transformations else sample[0],
-                self.text_transformations(sample[1]) if self.text_transformations else sample[1],
-                self.audio_transformations(sample[2]) if self.audio_transformations else sample[2]
+                self.text_transformations(sample[1]) if self.text_transformations else sample[1]
             )
 
             neg_sample = sorted([
                 pair for pair in range(len(batch_labels))
                 if pair != idx and batch_labels[idx] != batch_labels[pair]],
                 key=lambda sample_idx: self.pair_similarity_metric(sample, batch_data[sample_idx])
-            )[:1]
+            )[0]
 
             output_samples.append(
                 (
@@ -113,15 +96,29 @@ class SupervisedContrastSampler(BaseSampler):
     def __init__(self, **kwargs):
         super(SupervisedContrastSampler, self).__init__(**kwargs)
 
-    def pair_similarity_metric(self, pair1, pair2):
-        video_sim = similarity.SSIM().compute(img1=pair1[0], img2=pair2[0])
-        text_sim = similarity.calculate_text_similarity(pair1[1], pair2[1])
-        audio_sim = similarity.calculate_audio_similarity(pair1[-1], pair2[-1])
-        return video_sim + text_sim + audio_sim
+    def pair_similarity_metric(self, 
+        pair1: torch.Tensor, # either image or text sentence
+        pair2: torch.Tensor, # either image or text sentence
+        data_type: typing.Literal['image', 'text']
+    ):
+        if data_type == 'image':
+            ssim = similarity.SSIM().compute(img1=pair1, img2=pair2)
+        elif data_type == 'text':
+            ssim = similarity.calculate_text_similarity(pair1, pair2)
+        return ssim
         
     def hard_mining(self, batch_data: typing.List, batch_labels: typing.List):
+        """
+        Finds hard mining samples to enhance training
+        of the classifier.
+        
+        Parameters:
+        ----------
+            batch_data - data, which belongs to a single modality
+            batch_labels - corresponding list of labels.
+        """
         output_samples = []
-
+ 
         for idx, sample in enumerate(batch_data):
 
             pos_sample = sorted([
@@ -129,14 +126,14 @@ class SupervisedContrastSampler(BaseSampler):
                 if pair != idx and batch_labels[idx] == batch_labels[pair]
             ],
                 key=lambda sample_idx: self.pair_similarity_metric(sample, batch_data[sample_idx])
-            )[:1]
+            )[0]
 
             neg_sample = sorted([
                 pair for pair in range(len(batch_data)) 
                 if pair != idx and batch_labels[idx] != batch_labels[pair]
             ],
                 key=lambda sample_idx: self.pair_similarity_metric(sample, batch_data[sample_idx])
-            )[:1]
+            )[0]
 
             output_samples.append(
                 (
@@ -145,5 +142,5 @@ class SupervisedContrastSampler(BaseSampler):
                     neg_sample
                 )
             )
-        return output_samples 
+        return output_samples
 
