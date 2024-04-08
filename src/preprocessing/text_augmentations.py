7d034d442096ad11615a4b838e3462d489fa370b
= <kirklimushin@gmail.com>
2024-04-08 22:16:35 +0300

    Added textual augmentations

--- src/preprocessing/text_augmentations.py
+++ src/preprocessing/text_augmentations.py
@@ -1,11 +1,29 @@
-from src.training.preprocessing import base
-import torchtext
+import nlpaug.augmenter.word as naw
+import nlpaug.augmenter.sentence as ses
+import nlpaug.flow as naf
 
-def get_train_text_augmentations():
-    return torchtext.Compose(
+def get_train_text_augmentations() -> naw.Pipeline:
+    """
+    Set of textual training augmentations
+    for enhancing and divesifying training data.
+    """
+    return naf.Sequential(
+        [
+            naw.SynonymAug(aug_p=1),
+            ses.RandomSentenceAug(aug_p=0.7),
+            naw.RandomWordAug(aug_p=0.6),
+        ]
     )
 
-def get_val_text_augmentations():
-    return torchtext.Compose(
-        
+def get_val_text_augmentations() -> naw.Pipeline:
+    """
+    Set of textual data augmentations
+    for enhancing and diversifying validation data.
+    """
+    return naf.Sequential(
+        [
+            naw.SynonymAug(aug_p=1),
+            ses.RandomSentenceAug(aug_p=0.5),
+            naw.RandomWordAug(aug=0.3),
+        ]
     )
