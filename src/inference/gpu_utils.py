d43c0c9e1a098bd0aab8134790dc465efcb0ad13
= <kirklimushin@gmail.com>
2024-04-08 17:19:32 +0300

    Added module for measuring GPU inference

--- src/inference/gpu_utils.py
+++ src/inference/gpu_utils.py
@@ -1,8 +1,17 @@
 import subprocess 
-import logging
 
 def fix_gpu_clock_speed(clock_speed: int, device_id: str):
-    pass 
+    """
+    Fixates GPU clock speed using
+    NVIDIA-SIM command tool.
+    NOTE:
+        only works for nvidia based GPUs
+    """
 
 def release_gpu_clock_speed(device_id: str):
-    pass
+    """
+    Releasess GPU clock speed back
+    to default using NVIDIA-SIM command util.
+    NOTE:
+        only works for nvidia based GPUs.
+    """
