ac8cad1d65919ec8ba6b738a62d2f96b8aa40c88
= <kirklimushin@gmail.com>
2024-04-16 12:30:09 +0300

    Moved tokenizaation phase to text augmentations file

--- src/preprocessing/text_augmentations.py
+++ src/preprocessing/text_augmentations.py
@@ -1,8 +1,21 @@
 import nlpaug.augmenter.word as naw
 import nlpaug.augmenter.sentence as ses
 import nlpaug.flow as naf
+import typing
 
-def get_train_text_augmentations() -> naw.Pipeline:
+class InputWordTokenizer(naw.Augmenter):
+    """
+    Custom augmentation, which encodes
+    processed text sequence into embedding vector
+    """
+    def __init__(self, tokenizer, **kwargs):
+        super(InputWordTokenizer, self).__init__(**kwargs)
+        self.tokenizer = tokenizer
+    
+    def augment(self, input_text: str):
+        return self.tokenizer(input_text)
+
+def get_train_text_augmentations(input_tokenizer: typing.callable) -> naw.Pipeline:
     """
     Set of textual training augmentations
     for enhancing and divesifying training data.
@@ -12,10 +25,12 @@ def get_train_text_augmentations() -> naw.Pipeline:
             naw.SynonymAug(aug_p=1),
             ses.RandomSentenceAug(aug_p=0.7),
             naw.RandomWordAug(aug_p=0.6),
+            InputWordTokenizer(tokenizer=input_tokenizer)
         ]
     )
+    
 
-def get_val_text_augmentations() -> naw.Pipeline:
+def get_val_text_augmentations(input_tokenizer: typing.Callable) -> naw.Pipeline:
     """
     Set of textual data augmentations
     for enhancing and diversifying validation data.
@@ -25,6 +40,7 @@ def get_val_text_augmentations() -> naw.Pipeline:
             naw.SynonymAug(aug_p=1),
             ses.RandomSentenceAug(aug_p=0.5),
             naw.RandomWordAug(aug=0.3),
+            InputWordTokenizer(tokenizer=input_tokenizer)
         ]
     )
     
