ceaad3aa9d4c14daa121b59f2fdbb19d0996d150
= <kirklimushin@gmail.com>
2024-04-17 20:07:49 +0300

    Added new prototype implementations of attention-based fusion layers, which leverage different processing strategies

--- src/multimodal/fusions/attention_fusion.py
+++ src/multimodal/fusions/attention_fusion.py
@@ -9,19 +9,21 @@ logger.addHandler(handler)
 
 class AttentionFusion(nn.Module):
     """
-    Implementation of the Attention-based
-    Fusion layer for aligning multimodal output 
+    Implementation of the vanilla Attention-based
+    Fusion for aligning multimodal output 
     data.
     
     Parameters:
     -----------
-        1. channel_to_encoder_dim - dictionary
-            which maps encoder to it's number of channels.
-            Example:
-                {'encoder1': 1024, 'encoder2': 512},
+        1. channel_to_encoder_dim - list of
+        the following format: [1, 2, 3], where 1, 2, 3 -> number
+        of output dimensions for each encoder.
+
+        2. fusion_weights (nn.Module) - in case 
+        model has been pretrained and we want to load it's state
     """
     def __init__(self, 
-        channel_to_encoder_dim: typing.Dict[str, int], 
+        channel_to_encoder_dim: typing.List, 
         fusion_weights: nn.Module = None
     ):
         super(AttentionFusion, self).__init__()
@@ -48,27 +50,66 @@ class AttentionFusion(nn.Module):
     def load_fusion_state(self, fusion_state: nn.Module):
         self.attention = self.attention.load_state_dict(state_dict=fusion_state)
         
-    def forward(self, embeddings: typing.Dict[str, torch.Tensor]):
+    def forward(self, embeddings: typing.List[torch.Tensor]):
         """
         NOTE:
             it is implied, that embeddings are already
             projected into the same latent space and
             their length is equal.
         """
-        emb_projections = list(embeddings.values())
-        concat_embs = torch.cat(
-            [embeddings[key] for key in sorted(embeddings.keys())],
-            dim=-1
-        )
+        concat_embs = torch.cat(embeddings, dim=-1)
         attention_weights = self.attention(concat_embs)
         output_projections: typing.List[torch.Tensor] = []
  
         for emb in range(len(attention_weights)):
-            output_proj = attention_weights[:, emb].unsqueeze(-1) * emb_projections[emb]
+            output_proj = attention_weights[:, emb].unsqueeze(-1) * embeddings[emb]
             output_projections.append(output_proj)
         
-        weighted_sum_tensor = torch.sum(torch.stack(output_projections), dim=0)
+        weighted_sum_tensor = torch.sum(
+            torch.stack(output_projections), 
+            dim=0
+        )
         return weighted_sum_tensor
 
 
+class VisualDotProductAttentionFusion(nn.Module):
+    """
+    Attention-based fusion, what uses attention mechanism
+    called "Visual dot product Attention".
+    
+    Paper: https://paperswithcode.com/method/dot-product-attention
 
+    """
+    def __init__(self, 
+        channels_to_dim: typing.Dict[str, int], 
+        fusion_weights: nn.Module = None
+    ):
+        super(VisualDotProductAttnetionFusion, self).__init__()
+
+class CoAttentionFusion(nn.Module):
+    """
+    Attention-based fusion, that uses attention mechanism
+    called "Co-Attention".
+    
+    Paper: https://www.researchgate.net/figure/The-structure-of-co-attention-mechanism_fig3_343606404
+
+    """
+    def __init__(self, 
+        channels_to_dim: typing.Dict[str, int],
+        fusion_weights: nn.Module = None
+    ):
+        super(CoAttentionFusion, self).__init__()
+
+class StackedVisualAttentionFusion(nn.Module):
+    """
+    Attention-based fusion, that uses attention mechanism 
+    called "Stacked Visual Attention".
+
+    Paper: https://www.researchgate.net/publication/343842731_Stack-VS_Stacked_Visual-Semantic_Attention_for_Image_Caption_Generation
+
+    """
+    def __init__(self, 
+        channels_to_dim: typing.Dict[str, int],
+        fusion_weights: nn.Module = None
+    ):
+        super(StackedVisualAttentionFusion, self).__init__()
